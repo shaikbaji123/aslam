c9e2ec38c6f21f9c94db2e10d92c166799e3c7f5
baji123 <shaikbaji123@gmail.com>
2017-05-15 15:27:51 +0530

    yyyyyyy

---
+++ src/main/java/aslam/com/App.java
@@ -0,0 +1,13 @@
+package aslam.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
